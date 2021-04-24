b919182bd02054c3df0e93df81dffb4257c6bed7
moonh8 <moonh8@mcmaster.ca>
2021-04-24 14:10:56 -0400

    Dynamic programming examples(Fibonacci)

--- Python/Algorithms/Extra/fib_dynamic.py
+++ Python/Algorithms/Extra/fib_dynamic.py
@@ -3,7 +3,15 @@ def fib(n):
         return n
     return fib(n-1) + fib(n-2)
 
-## @brief dynamic fibonacci function
+
+#  @brief dynamic fibonacci function
 #  @details Use the previous outputs to save runtime
 def fib_dynamic(n):
-    if
+    if n < 2:
+        return n
+
+    f = [0, 1]
+    for _ in range(n-1):
+        f.append(f[-1] + f[-2])
+
+    return f[-1]
