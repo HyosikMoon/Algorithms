070dd9acfbc041cac4898a5e47cf7df12855338b
moonh8 <moonh8@mcmaster.ca>
2021-05-11 01:27:15 -0400

    Update vertex_cover

--- Python/Algorithms/Advanced/vertex_cover/VertexCover.py
+++ Python/Algorithms/Advanced/vertex_cover/VertexCover.py
@@ -21,7 +21,7 @@ class Graph:
             self.adj[node2].remove(node1)
             self.adj[node1].remove(node2)
 
-    def remove_incident_edge(self, node):
+    def remove_incident_edges(self, node):
         for neighbour in self.adj[node]:
             self.adj[neighbour].remove(node)
         self.adj[node] = []
@@ -35,9 +35,10 @@ class Graph:
     def adj_nodes(self, node):
         return self.adj[node]
 
-    def has_no_edges(self, node):
-        if len(self.adj[node]) > 0:
-            return False
+    def has_no_edges(self):
+        for node in self.adj:
+            if len(self.adj[node]) > 0:
+                return False
         return True
 
     def copy(self):
@@ -49,3 +50,53 @@ class Graph:
 
     def number_of_nodes(self):
         return len(self.adj)
+
+
+## Vertex cover
+def vertex_cover(G):
+    vc = list(G.adj.keys())
+    nodes = power_set(vc)
+    for node in nodes:
+        cg = G.copy()
+        for elem in node:
+            cg.remove_incident_edges(elem)
+        if cg.has_no_edges() and len(node) < len(vc):
+            vc = node
+    return vc
+
+
+# powerset
+# is empty
+def is_vertex_cover(G, cover):                  # if is_vertex_cover(G, [0])
+    temp_G = G.copy()                           # temp_G = {0:[1,2,3], 1:[0,2], 2:[0,1], 3:[0]}
+    for node in cover:
+        temp_G.remove_incident_edges(node)      # temp_G.remove_incident_edges(0)
+    if temp_G.has_no_edges():                   # if temp_G.has_no_edges() true means, "cover" is a vertex_cover 
+        return True
+    return False
+
+def power_set(elements):
+    if elements == []:
+        return [[]]
+    return add_to_each(power_set(elements[1:]), elements[0]) + power_set(elements[1:])
+
+
+def add_to_each(sets, element):
+    my_sets = sets.copy()
+    for my_set in my_sets:
+        my_set.append(element)
+    return my_sets
+
+graph = Graph()
+graph.add_node(0)
+graph.add_node(1)
+graph.add_node(2)
+graph.add_node(3)
+
+graph.add_edge(0, 1)
+graph.add_edge(0, 2)
+graph.add_edge(0, 3)
+graph.add_edge(1, 2)
+
+vc = vertex_cover(graph)
+print(vc)
