31da7b71d83cfc6af239a27fbc1702d6fa7824d7
moonh8 <moonh8@mcmaster.ca>
2021-06-02 23:04:26 -0400

    update recursion

--- Python/Recursion/Principle_of_Recursion/SwapNodesInPairs.py
+++ Python/Recursion/Principle_of_Recursion/SwapNodesInPairs.py
@@ -10,37 +10,20 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        # if head == None:
-        #     return
-        
-        # temp = head.next
-        # next_head = head.next.next
-        # if head.next.next == None:
-        #     head.next = None
-        #     temp.next = head
-        # elif head.next.next.next == None:
-        #     head.next = None
-        #     temp.next = head
-        # else:
-        #     head.next = head.next.next.next
-        #     temp.next = head
-        
-        # return self.swapPairs(next_head)
-
-        # If the list has no node or has only one node left.
+        # Recursion
         if not head or not head.next:
             return head
-
-        # Nodes to be swapped
+        
         first_node = head
         second_node = head.next
 
-        # Swapping
-        first_node.next  = self.swapPairs(second_node.next)
+        first_node.next = self.swapPairs(second_node.next)
         second_node.next = first_node
 
-        # Now the head is the second node
         return second_node
+
+        # Iterative
+
 
 # test #
 
