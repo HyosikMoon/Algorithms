ba9312adaaae509f2fd2f7fc66265ad301201b66
moonh8 <moonh8@mcmaster.ca>
2021-06-04 17:11:49 -0400

    Update Iterative code

--- Python/Recursion/Principle_of_Recursion/SwapNodesInPairs.py
+++ Python/Recursion/Principle_of_Recursion/SwapNodesInPairs.py
@@ -10,19 +10,24 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        # # Recursion
+        ## Recursion solution
+        # # If the list has no node or has only one node left.
         # if not head or not head.next:
         #     return head
-        
+
+        # # Nodes to be swapped
         # first_node = head
         # second_node = head.next
 
-        # first_node.next = self.swapPairs(second_node.next)
+        # # Swapping
+        # first_node.next  = self.swapPairs(second_node.next)
         # second_node.next = first_node
 
+        # # Now the head is the second node
         # return second_node
 
-        # Recursive review1
+
+        ## Recursive review1
         if not head or not head.next:
             return head
         
@@ -34,7 +39,54 @@ class Solution(object):
 
         return second_node
 
-        # Iterative
+
+        ## Iteration Solution
+        # # Dummy node acts as the prevNode for the head node
+        # # of the list and hence stores pointer to the head node.
+        # dummy = ListNode(-1)
+        # dummy.next = head
+
+        # prev_node = dummy
+
+        # while head and head.next:
+
+        #     # Nodes to be swapped
+        #     first_node = head;
+        #     second_node = head.next;
+
+        #     # Swapping
+        #     prev_node.next = second_node
+        #     first_node.next = second_node.next
+        #     second_node.next = first_node
+
+        #     # Reinitializing the head and prev_node for next swap
+        #     prev_node = first_node
+        #     head = first_node.next
+
+        # # Return the new head node.
+        # return dummy.next
+
+
+        ## Iteration review 
+        dummy = ListNode(-1)
+        dummy.next = head
+
+        prev_node = dummy
+        while head and head.next:
+            # Setting
+            first_node = head
+            second_node = head.next
+
+            # Swap
+            prev_node.next = second_node
+            first_node.next = second_node.next
+            second_node.next = first_node
+            
+            # Preparation for next step
+            prev_node = first_node
+            head = first_node.next
+
+        return dummy.next
 
 
 # test #
