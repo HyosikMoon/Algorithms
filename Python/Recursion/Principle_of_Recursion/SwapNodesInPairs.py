ad94dcad4f1a3011fe09a9e4515c146988393493
moonh8 <moonh8@mcmaster.ca>
2021-06-03 22:53:54 -0400

    update recursion

--- Python/Recursion/Principle_of_Recursion/SwapNodesInPairs.py
+++ Python/Recursion/Principle_of_Recursion/SwapNodesInPairs.py
@@ -10,7 +10,19 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        # Recursion
+        # # Recursion
+        # if not head or not head.next:
+        #     return head
+        
+        # first_node = head
+        # second_node = head.next
+
+        # first_node.next = self.swapPairs(second_node.next)
+        # second_node.next = first_node
+
+        # return second_node
+
+        # Recursive review1
         if not head or not head.next:
             return head
         
