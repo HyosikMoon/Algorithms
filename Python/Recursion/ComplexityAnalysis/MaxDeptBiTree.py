2e1ab82742ae6200ae5b017dab30600b271c2fe2
moonh8 <moonh8@mcmaster.ca>
2021-06-11 21:47:16 -0400

    Update complexity

---
+++ Python/Recursion/ComplexityAnalysis/MaxDeptBiTree.py
@@ -0,0 +1,28 @@
+def sum_tail_recursion(ls):
+    """
+    :type ls: List[int]
+    :rtype: int, the sum of the input list.
+    """
+    def helper(ls, acc):
+        if len(ls) == 0:
+            return acc
+        # this is a tail recursion because the final instruction is a recursive call.
+        return helper(ls[1:], ls[0] + acc)
+    
+    return helper(ls, 0)
+
+sum_tail_recursion([0,1,2])
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
