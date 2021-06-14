90ea7cf4c48633ccd2cbcefebd038b644a60637a
moonh8 <moonh8@mcmaster.ca>
2021-06-13 22:51:15 -0400

    Update numIsland

--- Python/Interview/Google/NumberOfIslands.py
+++ Python/Interview/Google/NumberOfIslands.py
@@ -20,10 +20,18 @@ class Solution(object):
                     visited.append(curr)
 
                     # step1. add adj 1s
+                    # Right
                     if curr[1] < len(grid[i]) - 1 and grid[curr[0]][curr[1] + 1] == "1" and (curr[0], curr[1] + 1) not in visited:
                         waiting.append((curr[0], curr[1] + 1))
+                    # Down
                     if curr[0] < len(grid) - 1 and grid[curr[0] + 1][curr[1]] == "1" and (curr[0] + 1, curr[1]) not in visited:
                         waiting.append((curr[0] + 1, curr[1]))
+                    # Left
+                    if curr[1] > 0 and grid[curr[0]][curr[1] - 1] == "1" and (curr[0], curr[1] - 1) not in visited:
+                        waiting.append((curr[0], curr[1] - 1))
+                    # Up
+                    if curr[0] > 0 and grid[curr[0] - 1][curr[1]] == "1" and (curr[0] - 1, curr[1]) not in visited:
+                        waiting.append((curr[0] - 1, curr[1]))
 
                     # step2. pop waiting value -> curr
                     if len(waiting) != 0:
