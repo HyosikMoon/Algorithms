cec12314aeb93906675bf96c9f526447c31c081b
moonh8 <moonh8@mcmaster.ca>
2021-06-13 21:29:12 -0400

    Update

--- Python/Recursion/ComplexityAnalysis/Pow.py
+++ Python/Recursion/ComplexityAnalysis/Pow.py
@@ -7,39 +7,39 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        if x == 0:
-            return 0
-        if n == 0 or x == 1:
-            return 1
+        # if x == 0:
+        #     return 0
+        # if n == 0 or x == 1:
+        #     return 1
 
-        m = n // 2
-        out = 1.0
-        while m != 0:
-            if m > 0:
-                out = out * x
-                m -= 1
-            else:
-                out = out * 1.0 / x
-                m += 1
-        if n % 2 == 0:
-            return out * out
-        elif n > 0:
-            return x * out * out
-        else:
-            return x * out * out
-
-
-        # if n == 0:
-        #     return 1    
-        # if n == 1:
-        #     return x
-        # if n == -1:
-        #     return 1/x
-
-        # half = self.myPow(x, n/2)
+        # m = n // 2
+        # out = 1.0
+        # while m != 0:
+        #     if m > 0:
+        #         out = out * x
+        #         m -= 1
+        #     else:
+        #         out = out * 1.0 / x
+        #         m += 1
         # if n % 2 == 0:
-        #     return half * half
-        # return x * half * half
+        #     return out * out
+        # elif n > 0:
+        #     return x * out * out
+        # else:
+        #     return x * out * out
+
+
+        if n == 0:
+            return 1    
+        if n == 1:
+            return x
+        if n == -1:
+            return 1/x
+
+        half = self.myPow(x, n/2)
+        if n % 2 == 0:
+            return half * half
+        return x * half * half
 
 
 Solution().myPow(8.84372, -5)
