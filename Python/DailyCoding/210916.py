f0ab544c4b4c9f6b0b20e583b53e06354514c970
HyosikMoon <hyosik0317@gmail.com>
2021-09-18 15:48:10 -0400

    Daily coding LinkedList addition

--- Python/DailyCoding/210916.py
+++ Python/DailyCoding/210916.py
@@ -1,13 +1,13 @@
 
 class Solution:
-    def twoSum(self, nums, target):
+    def twoSum1(self, nums, target):
         
         for index_i, i in enumerate(nums):
             for index_j, j in enumerate(nums):
                 if (i + j) == target and index_i != index_j:
                     return [index_i ,index_j]
 
-    def twoSum(self, nums, target):
+    def twoSum2(self, nums, target):
         d = {}
         for i, n in enumerate(nums):
             m = target - n
@@ -17,5 +17,4 @@ class Solution:
                 d[n] = i
 
 obj = Solution()
-
-print(obj.twoSum([1,3,3], 6))
+print(obj.twoSum2([1,3,3], 6))
---
+++ Python/DailyCoding/210918.py
@@ -0,0 +1,54 @@
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        # l1V
+        d1 = len(l1) - 1
+        l1.reverse()
+        l1V = 0
+        for i, n in enumerate(l1):
+            l1V += pow(10, d1 - i) * n
+            
+        # l2V
+        d1 = len(l2) - 1
+        l2.reverse()
+        l2V = 0
+        for i, n in enumerate(l2):
+            l2V += pow(10, d1 - i) * n
+            
+        # l1V + l2V
+        val = l1V + l2V
+        
+        # l3
+        s_val = str(val)
+        output = []
+        for i, n in enumerate(s_val):
+            output.append(int(n))
+        output.reverse()
+        
+        return output
+
+sol = Solution()
+sol.addTwoNumbers([2,4,3], [5,6,4])
+sol.addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9])
+
+
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+#         carry = 0
+#         root = n = ListNode(0)
+#         while l1 or l2 or carry:
+#             v1 = v2 = 0
+#             if l1:
+#                 v1 = l1.val
+#                 l1 = l1.next
+#             if l2:
+#                 v2 = l2.val
+#                 l2 = l2.next
+#             carry, val = divmod(v1+v2+carry, 10)
+#             n.next = ListNode(val)
+#             n = n.next
+#         return root.next
