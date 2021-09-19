c1bf4468d7625471c94ae1364821da9be10dd2ef
HyosikMoon <hyosik0317@gmail.com>
2021-09-19 17:46:50 -0400

    Dailycoding single linkedlist addition

--- Python/DailyCoding/210918.py
+++ Python/DailyCoding/210918.py
@@ -31,24 +31,37 @@ sol.addTwoNumbers([2,4,3], [5,6,4])
 sol.addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9])
 
 
-# # Definition for singly-linked list.
-# # class ListNode:
-# #     def __init__(self, val=0, next=None):
-# #         self.val = val
-# #         self.next = next
-# class Solution:
-#     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-#         carry = 0
-#         root = n = ListNode(0)
-#         while l1 or l2 or carry:
-#             v1 = v2 = 0
-#             if l1:
-#                 v1 = l1.val
-#                 l1 = l1.next
-#             if l2:
-#                 v2 = l2.val
-#                 l2 = l2.next
-#             carry, val = divmod(v1+v2+carry, 10)
-#             n.next = ListNode(val)
-#             n = n.next
-#         return root.next
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution2:
+    def addTwoNumbers2(self, l1, l2):
+        carry = 0
+        root = n = ListNode(0)
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            carry, val = divmod(v1+v2+carry, 10)
+            n.next = ListNode(val)
+            n = n.next
+        return root.next
+
+sol2 = Solution2()
+l1 = ListNode(3)
+l1.next = ListNode(9)
+l1.next.next = ListNode(4)
+
+l2 = ListNode(1)
+l2.next = ListNode(8)
+l2.next.next = ListNode(7)
+
+sol2_val = sol2.addTwoNumbers2(l1, l2)
+# sol.addTwoNumbers2([9,9,9,9,9,9,9], [9,9,9,9])
