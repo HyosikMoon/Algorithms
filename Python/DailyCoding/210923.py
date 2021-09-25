834aaa156652f89cd57cfc182cc062154b79e9b6
HyosikMoon <hyosik0317@gmail.com>
2021-09-24 23:20:36 -0400

    Palindrome_O(n^2)_solution

--- Python/DailyCoding/210923.py
+++ Python/DailyCoding/210923.py
@@ -16,6 +16,7 @@ class Solution:
             # from i to j, slice s -> sample
             # compare left and right of the sample
             # repeat until j reach to len(s)
+            # O(n^3)
             while j <= lim:
                 sample = s[i:j]
                 m = len(sample) // 2
@@ -39,5 +40,30 @@ class Solution:
         
         return w
 
-sol = Solution()
-sol.longestPalindrome("zdefeeeeeeeeasd")
+    # O(n^2)
+    def longestPalindrome2(self, s):
+        res = ""
+        for i in range(len(s)):
+            # odd case, like "aba"
+            tmp = self.helper(s, i, i)
+            if len(tmp) > len(res):
+                res = tmp
+            # even case, like "abba"
+            tmp = self.helper(s, i, i+1)
+            if len(tmp) > len(res):
+                res = tmp
+        return res
+    
+    # get the longest palindrome, l, r are the middle indexes   
+    # from inner to outer
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1; r += 1
+        return s[l+1:r]
+
+
+# sol = Solution()
+# sol.longestPalindrome("zdefeeeeeeeeasd")
+
+sol2 = Solution()
+sol2.longestPalindrome2("abcba")
