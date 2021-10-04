c64acfde8940551abcba9233f90c67c1ef3df4cb
HyosikMoon <hyosik0317@gmail.com>
2021-10-03 23:29:36 -0400

    Update zigzagConvert

--- Python/DailyCoding/210927.py
+++ Python/DailyCoding/210927.py
@@ -10,36 +10,37 @@
 # string convert(string s, int numRows);
  
 class Solution:
-    def convert(self, s, numRows):
+    # def convert(self, s, numRows):
         # while r < len(s):
         #   a = ['','', ... ,'',''] // an
         #   a0, a1, ... , a_(n-1), a_(n-2), ... , 
         #   s[0], s[1], ... s[n-1], s[n-2], s[n-3], ... , s[0]
         #   a0 ++ a1 ++ ... ++ an
+        # 
+        # 
 
 
 
 
+# class Solution2(object):
+    def convert2(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s):
+            return s
 
-# class Solution(object):
-#     def convert(self, s, numRows):
-#         """
-#         :type s: str
-#         :type numRows: int
-#         :rtype: str
-#         """
-#         if numRows == 1 or numRows >= len(s):
-#             return s
+        L = [''] * numRows
+        index, step = 0, 1
 
-#         L = [''] * numRows
-#         index, step = 0, 1
+        for x in s:
+            L[index] += x
+            if index == 0:
+                step = 1
+            elif index == numRows -1:
+                step = -1
+            index += step
 
-#         for x in s:
-#             L[index] += x
-#             if index == 0:
-#                 step = 1
-#             elif index == numRows -1:
-#                 step = -1
-#             index += step
-
-#         return ''.join(L)
+        return ''.join(L)
