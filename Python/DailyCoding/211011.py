7cb6efd405c163b92adb6c602d6f439b1a2b19e5
HyosikMoon <hyosik0317@gmail.com>
2021-10-11 19:36:28 -0400

    DailyCoding_StringToInteger_Regrex

--- Python/DailyCoding/211011.py
+++ Python/DailyCoding/211011.py
@@ -9,6 +9,93 @@
 # If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
 # Return the integer as the final result.
 
+
+# https://www.w3schools.com/python/python_regex.asp
+# https://leetcode.com/problems/string-to-integer-atoi/discuss/4653/Python-solution-based-on-RegEx
 class Solution:
-    def myAtoi(self, s: str) -> int:
-        
+    def myAtoi(self, s):
+        digit = "0123456789"
+        start = 0
+        end = 0
+        sign = 1
+
+        # Initial condition
+        if len(s) == 0:
+            return 0
+        elif s == " ":
+            return 0
+        elif s == "-":
+            return 0
+        elif s == "+":
+            return 0
+        elif "+-" in s or "-+" in s:
+            return 0
+
+        for i, c in enumerate(s):
+            if c != '+' and c != ' ' \
+                and c != '-' and c not in digit:
+                return 0
+            elif c in digit:
+                start = i
+                break
+            else:
+                continue
+
+        revIndex = -1
+        n = 0
+        while n < len(s):
+            c = s[revIndex]
+            if c in digit:
+                end = revIndex
+                break
+            revIndex -= 1
+            n += 1
+
+        # Check the sign
+        if start > 0 and s[start-1] == '-':
+            sign = -1
+
+        # Remove initial 0s
+        while s[start] == '0':
+            start += 1
+
+        nlmt = -2**31
+        plmt = 2**31 - 1
+        if end == -1:
+            out = sign * float(s[start:])
+            if out < nlmt:
+                out = nlmt
+            elif out > plmt:
+                out = plmt
+            else:
+                return round(out)
+        else:
+            out = sign * float(s[start:end+1])
+            if out < nlmt:
+                out = nlmt
+            elif out > plmt:
+                out = plmt
+            else:
+                return round(out)
+        return round(out)
+sol = Solution()
+sol.myAtoi("+1")
+
+
+# Solution
+    # def atoi(self, str):
+    #     str = str.strip()
+    #     str = re.findall('(^[\+\-0]*\d+)\D*', str)
+
+    #     try:
+    #         result = int(''.join(str))
+    #         MAX_INT = 2147483647
+    #         MIN_INT = -2147483648
+    #         if result > MAX_INT > 0:
+    #             return MAX_INT
+    #         elif result < MIN_INT < 0:
+    #             return MIN_INT
+    #         else:
+    #             return result
+    #     except:
+    #         return 0
