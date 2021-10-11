be74817fd2082e89c2bee053d4f9d174cdd8216c
HyosikMoon <hyosik0317@gmail.com>
2021-10-11 16:59:46 -0400

    DailyCoding_ReverseInteger

--- Python/DailyCoding/211009.py
+++ Python/DailyCoding/211009.py
@@ -15,3 +15,35 @@
 
 class Solution:
     def reverse(self, x):
+        sign = 0
+        if x < 0:
+            sign = 1
+        
+        if sign == 1:
+            x = str(x)[1:]
+        else:
+            x = str(x)
+            
+        out = ""
+        for c in x:
+            out = c + out
+        
+        if sign == 1:
+            out = -1 * int(out)
+        else:
+            out = int(out)
+
+        if out < -2**31 or out > (2**31 - 1):
+            return 0
+        else:
+            return out
+        
+
+    # def reverse(self, x):
+    #     """
+    #     :type x: int
+    #     :rtype: int
+    #     """
+    #     sign = [1,-1][x < 0]
+    #     rst = sign * int(str(abs(x))[::-1])
+    #     return rst if -(2**31)-1 < rst < 2**31 else 0
