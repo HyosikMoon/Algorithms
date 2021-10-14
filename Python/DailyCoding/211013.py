461e073ba6b951cc64d2c0e2869cfb09a58b6827
HyosikMoon <hyosik0317@gmail.com>
2021-10-14 00:59:26 -0400

    Solutions_Palindrome

--- Python/DailyCoding/211013.py
+++ Python/DailyCoding/211013.py
@@ -28,3 +28,21 @@ class Solution:
                     return False
                 i += 1
             return True
+
+#Solution1
+        # if x < 0:
+        #     return False
+        
+        # return str(x) == str(x)[::-1]
+
+
+            
+#Solution2
+        # if x<0:
+        #     return False
+        # inputNum = x
+        # newNum = 0
+        # while x>0:
+        #     newNum = newNum * 10 + x%10
+        #     x = x//10
+        # return newNum == inputNum
