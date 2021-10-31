8d2806bf1c907e77c8d991355cf49ac060145482
HyosikMoon <hyosik0317@gmail.com>
2021-10-30 22:56:38 -0400

    DailyCoding_maxArea

--- Python/DailyCoding/211029.py
+++ Python/DailyCoding/211029.py
@@ -3,4 +3,21 @@
 #Notice that you may not slant the container.
 
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
+    def maxArea(self, height):
+        # Find the max area
+        # O(n!) 
+        # area_height =  min(y1=height[i1], y2=height[i2]), length = i2 - i1
+        
+        area = 0
+        for i, y1 in enumerate(height):
+            for j, y2 in enumerate(height[i+1:]):
+                area_length = (j+1)
+                area_height = min(y2, y1)
+                sub_area = area_length * area_height
+                if sub_area > area:
+                    area = sub_area
+
+        return area
+
+sol = Solution()
+sol.maxArea([1,8,6,2,5,4,8,3,7])
