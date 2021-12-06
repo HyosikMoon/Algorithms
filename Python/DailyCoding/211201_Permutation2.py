992bd059bdd97533ba954bc0306a96ef831cee9d
HyosikMoon <hyosik0317@gmail.com>
2021-12-05 23:01:33 -0500

    DailyCoding_Permutation2

--- Python/DailyCoding/211201_Permutation2.py
+++ Python/DailyCoding/211201_Permutation2.py
@@ -16,23 +16,81 @@
 
 class Solution:
     def checkInclusion(self, s1, s2):
+        # # Solution 1 - Time Over, O(len(s1)*len(s2))
+        # # Compare every s2 in s1 from each index.
+        # length = len(s1)
+        # for i, c in enumerate(s2):
+        #     # Variable setting
+        #     word = s2[i:i+length]
+        #     dict1, dict2 = {}, {}
+
+        #     # Make two dicts for compare s1 and the word in s2
+        #     for char in s1:
+        #         dict1[char] = dict1.get(char, 0) + 1
+        #     for char in word:
+        #         dict2[char] = dict2.get(char, 0) + 1
+            
+        #     # Compare two dicts
+        #     if dict1 == dict2:
+        #         return True
         
+        # # There is no permuation in s2.
+        # return False
+
+
+        # # Solution 2 - Time Over, O(len(s1)*len(s2))
+        # length = len(s1)
+        # for i, c in enumerate(s2):
+        #     word = s2[i:i+length]
+        #     if sorted(s1) == sorted(word):
+        #         return True
+        # return False
+
+
+        # Solution 3 - A little bit faster but still quite late
         # Compare every s2 in s1 from each index.
         length = len(s1)
+
+        # Count s1 elements
+        dict1 = {}
+        for char in s1:
+            dict1[char] = dict1.get(char,0) + 1
+
+        # Count s2 elements and compare it with dict1
         for i, c in enumerate(s2):
             # Variable setting
             word = s2[i:i+length]
-            dict1, dict2 = {}, {}
+            dict2 = {}
 
             # Make two dicts for compare s1 and the word in s2
-            for char in s1:
-                dict1[char] = dict1.get(char, 0) + 1
             for char in word:
                 dict2[char] = dict2.get(char, 0) + 1
             
             # Compare two dicts
             if dict1 == dict2:
                 return True
-        
+
         # There is no permuation in s2.
         return False
+
+
+        # # Solution 4 
+        # A = [ord(x) - ord('a') for x in s1]
+        # B = [ord(x) - ord('a') for x in s2]
+        
+        # target = [0] * 26
+        # for x in A:
+        #     target[x] += 1
+        
+        # window = [0] * 26
+        # for i, x in enumerate(B):
+        #     window[x] += 1
+        #     if i >= len(A):
+        #         window[B[i - len(A)]] -= 1
+        #     if window == target:
+        #         return True
+        # return False
+
+
+sol = Solution()
+sol.checkInclusion('ab','eidbaooo')
