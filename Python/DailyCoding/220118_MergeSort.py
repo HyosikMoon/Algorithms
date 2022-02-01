8f2ad2e1201932db472c66271d8de79a49bafb96
HyosikMoon <hyosik0317@gmail.com>
2022-02-01 01:16:31 -0500

    DailyCoding_MergeSort

--- Python/DailyCoding/220118_MergeSort.py
+++ Python/DailyCoding/220118_MergeSort.py
@@ -27,39 +27,45 @@
 
 class Solution:
     def merge(self, arr, l, m, r):
+        if len(arr) == 1:
+            return 0
         il = l
-        k = 0
+        k, z1, z2 = 0, 0, 0
         ir = m+1
         comp1 = arr[l]
         comp2 = arr[m+1]
         while(il <= m and ir <= r):
             if comp1 <= comp2:
                 temp = arr[k]
+                z1 = ir
+                if (arr[k] != comp1):
+                    arr[z1] = temp
                 arr[k] = comp1
-                arr[m+1 + il] = temp
                 il += 1
                 k += 1
-                if (il <= m-1):
-                    comp1 = arr[m+1 + il]
+                if (il <= m):
+                    comp1 = arr[il]
             else:
                 temp = arr[k]
                 arr[k] = comp2
                 arr[ir] = temp
                 k += 1
                 ir += 1
-                if (ir <= r-1):
+                if (ir <= r):
                     comp2 = arr[ir]
 
     def mergeSort(self, arr, l, r):
+        if len(arr) == 1:
+            return 0
         if (l < r):
             m = int((l+r)/2)
             print(m)
             self.mergeSort(arr[l:m+1], l, m)
-            self.mergeSort(arr[m:r+1], m+1, r)
+            self.mergeSort(arr[m+1:r+1], m+1, r)
             self.merge(arr, l, m, r)
 
 sol = Solution()
-a = [3,5,1,2,6]
+a = [9,5]
 # sol.mergeSort(a,0,4)
 sol.merge(a,0,2,4)
 a
