ca061425d61d3996a47d7ed1aaa995dfb5af87da
moon <hyosik0317@gmail.com>
2022-02-20 10:58:17 -0400

    BinaryParser

---
+++ Python/DailyCoding/220220_BinaryParser.py
@@ -0,0 +1,22 @@
+src: str; pos: int; sym: str
+
+def nxt():
+    global pos, sym
+    if pos < len(src): sym, pos = src[pos], pos + 1
+    else: sym = chr(0) # end of input symbol
+
+def binary(): # binary → digit { digit }
+    digit()
+    while sym in '01': digit()
+
+def digit(): # digit → '0' | '1'
+    if sym == '0': nxt()
+    elif sym == '1': nxt()
+    else: raise Exception("invalid character at " + str(pos))
+
+def parse(s: str):
+    global src, pos;
+    src, pos = s, 0; nxt(); binary()
+    if sym != chr(0): raise Exception("unexpected character at " + str(pos))
+
+parse("101")
