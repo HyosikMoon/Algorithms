48d4595b5403ed6fac678bc91023737bb1d9a4da
Hyosik Moon <moonh8@mcmaster.ca>
2022-06-04 16:42:37 +0900

    4sum

--- Python/DailyCoding/220520_Letter_Combination.py
+++ Python/DailyCoding/220520_Letter_Combination.py
@@ -1,25 +1,43 @@
 class Solution(object):
     def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
-        if len(digits) ==0:
-            return []
+    #     """
+    #     :type digits: str
+    #     :rtype: List[str]
+    #     """
+    #     if len(digits) ==0:
+    #         return []
 
-        dict = { "2": "abc", "3": "def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
-        res=[]
+    #     dict = { "2": "abc", "3": "def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
+    #     res=[]
             
+    #     self.dfs(digits, 0, dict, '', res)
+    #     return res
+    
+    # def dfs(self, digits, index, dict, path, res):
+    #     if index >= len(digits):
+    #         res.append(path)
+    #         return
+    #     strings = dict[digits[index]]
+    #     for char in strings:
+    #         self.dfs(digits, index+1, dict, path + char, res)
+
+        dict = {'2':'abc', '3':'def', "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
+        res = []
+
+        if len(digits) == 0:
+            return []
+        
         self.dfs(digits, 0, dict, '', res)
         return res
-    
+
     def dfs(self, digits, index, dict, path, res):
         if index >= len(digits):
             res.append(path)
             return
-        strings = dict[digits[index]]
-        for char in strings:
-            self.dfs(digits, index+1, dict, path + char, res)
+
+        str = dict[digits[index]]
+        for char in str:
+            self.dfs(digits, index+1, dict, path+char, res) 
 
 sol = Solution()
 print(sol.letterCombinations('23'))
---
+++ Python/DailyCoding/220524_Numpy.py
@@ -0,0 +1,4 @@
+import numpy as np
+from pandas import array
+
+a = np.array([1,2,3])
