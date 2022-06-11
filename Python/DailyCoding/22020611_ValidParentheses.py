b5c389f68e644c473c9fbb12b1027a7dbb7a6e4b
Hyosik Moon <moonh8@mcmaster.ca>
2022-06-11 18:58:39 +0900

    ValidParentheses

---
+++ Python/DailyCoding/22020611_ValidParentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, str):
+        stack = []
+        dict = {'(':')', '[':']', '{':'}'}
+
+        for i in range(len(str)):
+            if str[i] in dict.keys():
+                stack.append(str[i])
+            else:
+                if stack == [] or dict[stack.pop()] != str[i]:
+                    return False
+        
+        return len(stack) == 0
