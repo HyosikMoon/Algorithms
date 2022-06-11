ee3f4dc1c647f903900192b2cd4a3099310a15c6
Hyosik Moon <moonh8@mcmaster.ca>
2022-06-11 16:49:34 +0900

    4sum

--- Python/DailyCoding/220602_4sum.py
+++ Python/DailyCoding/220602_4sum.py
@@ -30,39 +30,75 @@
 #           results.append([nums[i]] + item)
 #     return results
 
+# class Solution:
+#   def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+#     results = []
+#     nums.sort()
+#     for i in range(len(nums)-3):
+#       if i != 0 and nums[i] == nums[i-1]:
+#         continue
+#       t = target - nums[i]
+#       result = self.threeSum(nums[i+1:], t)
+#       # print(result)
+#       for term in result:
+#         results.append(term + [nums[i]])
+#     return results
+
+#   def threeSum(self, nums, target):
+#     results = []
+#     nums.sort()
+#     for i in range(len(nums)-2):
+#       if i != 0 and nums[i] == nums[i-1]:
+#         continue
+#       l = i+1; r = len(nums)-1
+#       while l < r:
+#         s = nums[i] + nums[l] + nums[r]
+#         if s == target:
+#           results.append([nums[i], nums[l], nums[r]])
+#           while l<r and nums[l] == nums[l+1]: l += 1
+#           while l<r and nums[r] == nums[r-1]: r -= 1
+#           l += 1; r -= 1
+#         elif s < target:
+#           while l<r and nums[l] == nums[l+1]: l += 1
+#           l += 1
+#         else:
+#           while l<r and nums[r] == nums[r-1]: r -= 1
+#           r -= 1
+#     return results
+
 class Solution:
-  def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-    results = []
-    nums.sort()
-    for i in range(len(nums)-3):
-      if i != 0 and nums[i] == nums[i-1]:
-        continue
-      t = target - nums[i]
-      result = self.threeSum(nums[i+1:], t)
-      # print(result)
-      for term in result:
-        results.append(term + [nums[i]])
-    return results
-
   def threeSum(self, nums, target):
-    results = []
     nums.sort()
-    for i in range(len(nums)-2):
-      if i != 0 and nums[i] == nums[i-1]:
-        continue
-      l = i+1; r = len(nums)-1
-      while l < r:
-        s = nums[i] + nums[l] + nums[r]
-        if s == target:
-          results.append([nums[i], nums[l], nums[r]])
-          while l<r and nums[l] == nums[l+1]: l += 1
-          while l<r and nums[r] == nums[r-1]: r -= 1
-          l += 1; r -= 1
-        elif s < target:
-          while l<r and nums[l] == nums[l+1]: l += 1
-          l += 1
-        else:
-          while l<r and nums[r] == nums[r-1]: r -= 1
-          r -= 1
+    results = []
+    for i in range(len(nums) - 2):
+      if i == 0 or nums[i] != nums[i-1]:
+        l = i+1; r = len(nums)-1
+        while l < r :
+          if target == nums[i] + nums[l] + nums[r]:
+            results.append([nums[i],nums[l],nums[r]])
+            while l<r and nums[r] == nums[r-1]: r -= 1
+            while l<r and nums[l] == nums[l+1]: l += 1
+            l += 1; r -= 1
+          elif target < nums[i] + nums[l] + nums[r]:
+            while l<r and nums[r] == nums[r-1]: r -= 1
+            r -= 1
+          else:
+            while l<r and nums[l] == nums[l+1]: l += 1
+            l += 1
     return results
 
+  def fourSum(self, nums, target):
+    nums.sort()
+    results = []
+    for i in range(len(nums) - 3):
+      if i == 0 or nums[i] != nums[i-1]:
+        newTarget = target - nums[i]
+        sums = self.threeSum(nums[i+1:], newTarget)
+        if sums != []:
+          for item in sums:
+            if [nums[i]]+item not in results:
+              results.append([nums[i]]+item)
+    return results
+
+sol = Solution()
+print(sol.fourSum([1,0,-1,0,-2,2], 0))
