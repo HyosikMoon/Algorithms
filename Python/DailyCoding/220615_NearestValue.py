72e53aac359a5ca9c6c14615d2447342b666b2bb
Hyosik Moon <moonh8@mcmaster.ca>
2022-06-15 16:28:00 +0900

    NearestValues

--- Python/DailyCoding/220615_NearestValue.py
+++ Python/DailyCoding/220615_NearestValue.py
@@ -1,15 +1,17 @@
 def nearest_value(values: set, one: int) -> int:
-    if one in values:
-        return one
-    
-    dict = {}
-    diff = 0
-    lst = list(values)
-    lst.sort()
-    for i, v in enumerate(lst):
-        diff = abs(v - one)
-        dict[i]=diff
-    
-    minIdx = list(dict.values()).index(min(dict.values()))
-    return lst[minIdx]
+    # if one in values:
+    #     return one
 
+    # dict = {}
+    # diff = 0
+    # lst = list(values)
+    # lst.sort()
+    # for i, v in enumerate(lst):
+    #     diff = abs(v - one)
+    #     dict[i]=diff
+    
+    # minIdx = list(dict.values()).index(min(dict.values()))
+    # return lst[minIdx]
+    return min(values, key=lambda x: abs(one-x))
+
+print(nearest_value({1,3,4,5,10}, 6))
